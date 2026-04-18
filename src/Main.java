725939d5ab65b6ec27ce34244cbb1858df2435bb
Jean Robert <jeanrobertjoseph@hotmail.com>
2026-04-18 03:08:40 -0400

    - Version finale complète du projet (premier commit) - Correction : génération du fichier resultats.csv (deuxième commit)

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         String fichierEntree = "data/etudiants.csv";
-        String fichierSortie = "output/resultats.csv";
+        String fichierSortie = "data/resultats.csv";
 
         try {
             List<Etudiant> etudiants = GestionCSV.lireEtudiantsDepuisCSV(fichierEntree);
